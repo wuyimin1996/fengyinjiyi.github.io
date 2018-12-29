2e4346bffbf0f3b758d867db8b7cd0a3900232a0
jun_jie_xu <964261897@qq.com>
2018-12-29 16:23:58 +0800

    Site updated: 2018-12-29 16:23:52

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,1 +1,18 @@
-var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/uploadsImage/favicon_err.ico"),document.title="页面崩溃啦 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/uploadsImage/favicon_back.ico"),document.title="突然又好了~"+OriginTitle,titleTime=setTimeout(function(){$('[rel="icon"]').attr("href","/images/favicon-32×32.ico"),document.title=OriginTitle},2e3))});
+//崩溃欺骗
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/uploadsImage/favicon_err.ico");
+        document.title = '页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/uploadsImage/favicon_back.ico");
+        document.title = '突然又好了~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            $('[rel="icon"]').attr('href', "/images/favicon-32×32.ico");
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
